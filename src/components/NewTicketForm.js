7eca0060e0651874cffdc15f13b365dc58115048
Megan McKissack <meganmckissack@gmail.com>
2022-11-14 19:42:12 -0800

    add firebase queries and ticket timing

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -2,6 +2,7 @@ import React from "react";
 // import { v4 } from 'uuid';
 import PropTypes from "prop-types"; 
 import ReusableForm from "./ReusableForm";
+import { serverTimestamp } from "firebase/firestore";
 
 function NewTicketForm(props){
 
@@ -11,6 +12,7 @@ function NewTicketForm(props){
       names: event.target.names.value, 
       location: event.target.location.value, 
       issue: event.target.issue.value, 
+      timeOpen: serverTimestamp()
       // id: v4()
     });
   }
